e7617a6e17fc38115672d80b922edb7a91b38b46
Pushkar Bhardwaj <cadetmike01@gmail.com>
2025-05-08 17:11:29 +0530

    Problem 1 solved

---
+++ Add two numbers.cpp
@@ -0,0 +1,19 @@
+//given two linked lists with numbers, consider them as numbers and reverse them, add them and again reverse and make a new linked list.
+//eg. l1=[2,4,3], l2=[5,6,4] thus l3=[7,0,8]
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+    public:
+        int left, right;
+        node*left= NULL;
+        node*right=NULL;       
+};
+
+int main()
+{
+    int n,m;
+    node*head=NULL;
+}
