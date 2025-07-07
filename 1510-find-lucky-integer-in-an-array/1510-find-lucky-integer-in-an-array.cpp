08792d2ca43f6a1b921addfa9b7596f822b5df42
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-07-07 19:14:47 +0530

    Time: 0 ms (100%), Space: 13.8 MB (89.59%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
@@ -1,45 +1,16 @@
 class Solution {
 public:
-    void merge(vector<int>& arr, int left, int mid, int right) {
-        vector<int> temp;
-        int i = left, j = mid + 1;
-        while (i <= mid && j <= right) {
-            if (arr[i] > arr[j]) temp.push_back(arr[i++]);
-            else temp.push_back(arr[j++]);
-        }
-        while (i <= mid) temp.push_back(arr[i++]);
-        while (j <= right) temp.push_back(arr[j++]);
-        for (int k = 0; k < temp.size(); k++) {
-            arr[left + k] = temp[k];
-        }
-    }
-    void mergeSort(vector<int>& arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
-    }
     int findLucky(vector<int>& arr) {
-        int n = arr.size();
-        mergeSort(arr, 0, n - 1);
-        bool found = 0;
-        int i = 0;
-        while (!found && i < n) {
-            int a = arr[i];
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == a) {
-                    count++;
-                }
-            }
-            if (count == a) {
-                found = 1;
-                return a;
-            }
-            i++;
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
         }
-        return -1;
+        int result = -1;
+        for (auto& [num, count] : freq) {
+            if (num == count) {
+                result = max(result, num);
+            }
+        }
+        return result;
     }
 };
