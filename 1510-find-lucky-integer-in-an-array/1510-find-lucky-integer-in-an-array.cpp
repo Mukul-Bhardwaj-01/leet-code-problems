dba53016fc19cfe22648b40f698dad2860d4e0f9
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-07-07 19:11:13 +0530

    Time: 25 ms (3.36%), Space: 23 MB (13.2%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
-    int findLucky(vector<int>& arr) {
-        int n=arr.size();
-        for(int i=0; i<n-1; i++) {
-            for(int j=0; j<n-i-1; j++) {
-                if(arr[j]<arr[j+1]) swap(arr[j],arr[j+1]);
-            }
+    void merge(vector<int>& arr, int left, int mid, int right) {
+        vector<int> temp;
+        int i = left, j = mid + 1;
+        while (i <= mid && j <= right) {
+            if (arr[i] > arr[j]) temp.push_back(arr[i++]);
+            else temp.push_back(arr[j++]);
         }
-        bool found =0;
-        int i=0;
-        while (!found && i<n) {
+        while (i <= mid) temp.push_back(arr[i++]);
+        while (j <= right) temp.push_back(arr[j++]);
+        for (int k = 0; k < temp.size(); k++) {
+            arr[left + k] = temp[k];
+        }
+    }
+    void mergeSort(vector<int>& arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+    int findLucky(vector<int>& arr) {
+        int n = arr.size();
+        mergeSort(arr, 0, n - 1);
+        bool found = 0;
+        int i = 0;
+        while (!found && i < n) {
             int a = arr[i];
-            int count =0;
-            for (int j=0; j<n; j++) {
-                if(arr[j]==a) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == a) {
                     count++;
-                }                 
+                }
             }
-            if(count==a) {
+            if (count == a) {
                 found = 1;
                 return a;
             }
