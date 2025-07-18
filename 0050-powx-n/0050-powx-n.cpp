ef05f7eb4694ad07dde17811a4769daa4f12bd6c
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-07-19 00:14:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (42.31%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        long long p=n;
+        if(p<0) p=-p;
+        double res=1.0;
+        while(p>0) {
+            if(p%2!=0) {
+                res*=x;
+            }
+            x*=x;
+            p/=2.0;
+        }
+        if(n>0) return res;
+        return 1/res;
+    }
+};
+
+/*double res= 1.0;
+        if(x==1) return 1;
+        long long m=abs(n);
+        for(int i=0; i<m; i++) {
+            res*=x;
+        }
+        if(n>0) return res;
+        else {
+            res=1/res;
+            return res;
+        }
+        return 0;*/
