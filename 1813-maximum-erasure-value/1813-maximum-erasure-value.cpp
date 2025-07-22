0bb11258ccc29040cb53120dbd18faca614f73dc
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-07-23 00:25:26 +0530

    Time: 186 ms (45.98%), Space: 131.1 MB (31.9%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         unordered_set<int> seen;
-        if (nums.size() == 1) {
-            return nums[0];
-        }
         int left = 0, right = 0, cursum = 0, maxsum = 0;
         while (right < nums.size()) {
             if (!seen.count(nums[right])) {
