8ea50da357578a5b2448d7992697dc717268e7b0
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-07-25 20:25:37 +0530

    Time: 0 ms (100%), Space: 22.6 MB (78.57%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,8 +4,7 @@ public:
         int i=0;
         for(int j=1;j<nums.size();++j) {
             if(nums[j]!=nums[i]) {
-                i++;
-                nums[i]=nums[j];
+                nums[++i]=nums[j];
             }
         }
         return i+1;
