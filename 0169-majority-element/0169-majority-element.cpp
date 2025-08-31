f671a17853bbe443f70389edd60b5ad3af7a7f58
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-09-01 00:56:34 +0530

    Time: 0 ms (100%), Space: 28 MB (86.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        map<int,int> hash;
-        for(int i:nums) hash[i]++;
-        for(auto it:hash) {
-            if(it.second>n/2) {
-                return it.first;
+        int el, count=0;
+        for(int i=0; i<n; ++i) {
+            if(count==0) {
+                count++;
+                el = nums[i];
             }
+            else if(nums[i]==el) count++;
+            else count--;
         }
-        return 0; 
+        return el;
     }
 };
