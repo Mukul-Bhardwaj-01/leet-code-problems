fc9731a2ef28f472303a13afddc7090008fe50c1
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-09-26 19:55:22 +0530

    Time: 1 ms (18.41%), Space: 9.7 MB (64.57%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
+    int funcNCR(int n,int r) {
+        long long res = 1;
+        for(int i=0; i<r; ++i) {
+            res = res*(n-i);
+            res = res/(i+1);
+        }
+        return res;
+    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
-
-        // Add the first row
-        res.push_back({1});
-
-        for (int i = 1; i < numRows; ++i) {
-            vector<int> row(i + 1, 1);  // Initialize row with 1s
-            for (int j = 1; j < i; ++j) {
-                row[j] = res[i - 1][j - 1] + res[i - 1][j];
+        for(int i=1; i<=numRows; ++i) {
+            vector<int> temp;
+            for(int j=1; j<=i; ++j) {
+                temp.emplace_back(funcNCR(i-1,j-1));
             }
-            res.push_back(row);
+            res.emplace_back(temp);
         }
-
         return res;
     }
 };
