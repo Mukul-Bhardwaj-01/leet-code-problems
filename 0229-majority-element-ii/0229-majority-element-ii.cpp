b536597ca4fd65b60ec1fce8a750f9b636c8c76b
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-09-27 21:15:58 +0530

    Time: 0 ms (100%), Space: 23.4 MB (99.87%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,12 +2,34 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> res;
-        int t = nums.size()/3;
-        map<long long, int> hash;
+        int el1, el2, count1 = 0, count2 = 0, n = nums.size();
         for(int i : nums) {
-            hash[i]++;
-            if (hash[i]>t && find(res.begin(), res.end(), i)==res.end()) res.emplace_back(i);
-            if(res.size()==2) break;
+            if(count1 == 0 && el2!=i) {
+                count1++; el1 = i;
+            }
+            else if(count2 == 0 && el1!=i) {
+                count2++; el2 = i;
+            }
+            else if(i == el1) count1++;
+            else if(i == el2) count2++;
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        if(count1!=0) {
+            int freq1 = 0;
+            for(int i : nums) {
+                if(i == el1) freq1++;
+            }
+            if(freq1>n/3) res.emplace_back(el1);
+        }
+        if(count2!=0) {
+            int freq2 = 0;
+            for(int i : nums) {
+                if(i == el2) freq2++;
+            }
+            if(freq2>n/3) res.emplace_back(el2);
         }
         return res;
     }
