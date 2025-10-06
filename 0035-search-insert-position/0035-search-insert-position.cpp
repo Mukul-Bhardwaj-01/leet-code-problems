364c12b91ab5946d6cd755b9d540aa0468a4087d
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-10-06 23:35:44 +0530

    Time: 0 ms (100%), Space: 13.6 MB (42.03%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& A, int t, int low, int high) {
-        int mid=(low+high)/2;
-        if(low>high) return low;
-        if(A[mid]==t) return mid;
-        else if(A[mid]<t) 
-            return binarySearch(A,t,mid+1,high);
-        else 
-            return binarySearch(A,t,low,mid-1);
-    }
     int searchInsert(vector<int>& nums, int target) {
-        int i=binarySearch(nums,target,0,nums.size()-1);
-        return i;
+        return (lower_bound(nums.begin(),nums.end(),target) - nums.begin());
     } 
 };
