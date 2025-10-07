ea252dd546b0859296660aa769844a1b7a1145f5
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-10-08 02:42:12 +0530

    Time: 0 ms (100%), Space: 18 MB (55.36%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -5,22 +5,22 @@ public:
             int mid = low + (high - low)/2;
             if(nums[mid] == t) return 1;
             if(nums[low] == nums[mid] && nums[mid] == nums[high]) {
-                low++; high--;
+                low++;
+                high--;
                 continue;
             }
-            if(low<=high) {
-                if(nums[low]<=nums[mid]) {
-                    if(nums[low]<=t && t<nums[mid]) high = mid - 1;
-                    else low = mid + 1;
-                }
-                else {
-                    if(nums[mid]<t && t<=nums[high]) low = mid + 1;
-                    else high = mid - 1;
-                }
+            if(nums[low]<=nums[mid]) {
+                if(nums[low]<=t && t<nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+            else {
+                if(nums[mid]<t && t<=nums[high]) low = mid + 1;
+                else high = mid - 1;
             }
         }
-        return 0;
+    return 0;
     }
+
     bool search(vector<int>& nums, int target) {
         return BS(nums, target, 0, nums.size()-1);
     }
