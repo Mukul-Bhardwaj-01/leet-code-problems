09bc3df4053bf9567696c8792c88357e31b4fee8
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-10-08 02:37:10 +0530

    Time: 3 ms (2.91%), Space: 17.9 MB (89.75%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        for(int i = 0; i<nums.size(); ++i) if(nums[i]==target) return 1;
+    int BS(vector<int>& nums, int t, int low, int high) {
+        while(low<=high) {
+            int mid = low + (high - low)/2;
+            if(nums[mid] == t) return 1;
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++; high--;
+                continue;
+            }
+            if(low<=high) {
+                if(nums[low]<=nums[mid]) {
+                    if(nums[low]<=t && t<nums[mid]) high = mid - 1;
+                    else low = mid + 1;
+                }
+                else {
+                    if(nums[mid]<t && t<=nums[high]) low = mid + 1;
+                    else high = mid - 1;
+                }
+            }
+        }
         return 0;
+    }
+    bool search(vector<int>& nums, int target) {
+        return BS(nums, target, 0, nums.size()-1);
     }
 };
