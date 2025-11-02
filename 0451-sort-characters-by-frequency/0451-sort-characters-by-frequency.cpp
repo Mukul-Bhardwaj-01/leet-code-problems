4128241ff17121d55278b9043f82e0a42364d8e4
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-11-02 20:54:01 +0530

    Time: 0 ms (100%), Space: 19.1 MB (6.56%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,27 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    static bool comp(pair<char,int>& p1, pair<char,int>& p2) {
-        return p1.second > p2.second;
-    }
     string frequencySort(string s) {
-        int n = s.length();
-        vector<pair<char, int>> hashvec; //vector to store char and their freq from s
-        for(char c : s) {
-            auto it = find_if(hashvec.begin(),hashvec.end(),
-            [c](const pair<char,int>& p) {return p.first == c;});
-            if(it != hashvec.end()) {
-                int indx = it - hashvec.begin();
-                hashvec[indx].second++;
-            }
-            else hashvec.push_back({c,1});
+        int n = s.size();
+        vector<int> freq(128, 0); // ASCII chars frequency
+        for(char c : s) freq[c]++;
+
+        // strfreq[i] holds characters that appear i times
+        vector<string> strfreq(n + 1);
+        for(int c = 0; c < 128; ++c) {
+            if(freq[c] > 0)
+                strfreq[freq[c]].push_back((char)c);
         }
-        vector<string> strfreq(n+1); //vector with ith index having char/s that appear i times
-        for(auto it : hashvec) strfreq[it.second] += it.first;
-        //traversing strfreq from back and adding char/s into result
+
         stringstream ss;
-        for(int i = n; i>0; --i) {
-            for(int j = 0; j<strfreq[i].length(); ++j) {
-                for(int k = 0; k<i; ++k) ss<<strfreq[i][j];
+        for(int i = n; i > 0; --i) {
+            for(char c : strfreq[i]) {
+                ss << string(i, c);
             }
         }
         return ss.str();
