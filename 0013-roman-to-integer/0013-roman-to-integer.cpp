93080a6168f4a76921e6889946d6ccdb1e7af1fe
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-11-04 23:38:41 +0530

    Time: 0 ms (100%), Space: 9.8 MB (99.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,66 +2,46 @@ class Solution {
 public:
     int romanToInt(string s) {
         int IntNum = 0;
-        int i=0;
-        while (i<s.length()) {
-            if(s[i]=='M') {
-                IntNum+=1000;
-                i++;
-            }
-            if(s[i]=='D') {
-                IntNum+=500;
-                i++;
-            }
-            if(s[i]=='C') {
-                if(s[i+1]=='M') {
+        int i=0, n = s.length();
+        while (i<n) {
+            if(s[i]=='M') IntNum+=1000;
+            else if(s[i]=='D') IntNum+=500;
+            else if(s[i]=='C') {
+                if(i+1<n && s[i+1]=='M') {
                     IntNum+=900;
-                    i+=2;
+                    i++;
                 }
-                else if(s[i+1]=='D') {
+                else if(i+1<n && s[i+1]=='D') {
                     IntNum+=400;
-                    i+=2;
-                }
-                else {
-                    IntNum+=100;
                     i++;
                 }
+                else IntNum+=100;
             }
-            if(s[i]=='L') {
-                IntNum+=50;
-                i++;
-            }
-            if(s[i]=='X') {
-                if (s[i+1]=='C') {
+            else if(s[i]=='L') IntNum+=50;
+            else if(s[i]=='X') {
+                if (i+1<n && s[i+1]=='C') {
                     IntNum+=90;
-                    i+=2;
+                    i++;
                 }
-                else if(s[i+1]=='L') {
+                else if(i+1<n && s[i+1]=='L') {
                     IntNum+=40;
-                    i+=2;
-                }
-                else {
-                    IntNum+=10;
                     i++;
                 }
+                else IntNum+=10;
             }
-            if(s[i]=='V') {
-                IntNum+=5;
-                i++;
-            }
-            if(s[i]=='I') {
-                if(s[i+1]=='V') {
+            else if(s[i]=='V') IntNum+=5;
+            else {
+                if(i+1<n && s[i+1]=='V') {
                     IntNum+=4;
-                    i+=2;
-                }
-                else if(s[i+1]=='X') {
-                    IntNum+=9;
-                    i+=2;
-                }
-                else {
-                    IntNum+=1;
                     i++;
                 }
+                else if(i+1<n && s[i+1]=='X') {
+                    IntNum+=9;
+                    i++;
+                }
+                else IntNum+=1;
             }
+            i++;
         }
         return IntNum;
     }
