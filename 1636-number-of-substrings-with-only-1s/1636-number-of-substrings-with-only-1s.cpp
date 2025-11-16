af426309fb04c0aed082370ee34d41b55e21d906
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-11-17 02:31:03 +0530

    Time: 0 ms (100%), Space: 11.4 MB (94.55%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    long long substrCount(int length) {
-        long long num = 0;
-        for(int i = 0; i<length; ++i) {
-            for(int j = i; j<length; ++j) num++;
-        }
-        return num;
-    }
     int numSub(string s) {
-        long long count = 0; int length = 0;
-        for(char c : s) {
-            if(c=='1') length++;
+        long long count = 0, length = 0;
+        for(int i = 0; i<=s.length(); ++i) {
+            if(s[i]=='1') length++;
             else if(length!=0) {
-                count+=substrCount(length);
+                long long substrnum = (length*length + length)/2;
+                count += substrnum;
                 length=0;
             }
         }
-        if(length!=0) count+=substrCount(length);
         return count%(1000000007);
     }
 };
