e67a29ae494294e421fa889cf54b988ca95ec665
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2025-11-17 02:40:37 +0530

    Time: 0 ms (100%), Space: 11.7 MB (38.86%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     int numSub(string s) {
         long long count = 0, length = 0;
-        const long mod = 1000000007;
-
         for (int i = 0; i < s.length(); ++i) {
             if (s[i] == '1') {
                 length++;
             } else if (length != 0) {
-                count = (count + length * (length + 1) / 2) % mod;
+                count = (count + length * (length + 1) / 2);
                 length = 0;
             }
         }
-
         if (length != 0) {
-            count = (count + length * (length + 1) / 2) % mod;
+            count = (count + length * (length + 1) / 2);
         }
-
-        return count % mod;
+        return count % 1000000007;
     }
 };
