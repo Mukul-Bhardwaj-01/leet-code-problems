c65718b1ece089320aab8e5ebe30d5537d6f96d1
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-03-13 21:07:58 +0530

    Time: 6 ms (89.26%), Space: 11.5 MB (52.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int a=0,b=0;
-        vector<pair<int,int>> dp;
-        for (int i=0; i<s.length(); i++) {
-            expandFromCenter(i,i,s,dp);
-            expandFromCenter(i,i+1,s,dp);
+        string t = "^";
+        for(char c : s) {
+            t += "#" + string(1,c);
         }
-        int max = dp[0].second - dp[0].first;
-        for(int j=1; j<dp.size(); j++) {
-            if(dp[j].second-dp[j].first>max) {
-                max= dp[j].second-dp[j].first;
-                a=dp[j].first;
-                b=dp[j].second;
+        t += "#$";
+        int n = t.size();
+        vector<int> P(n,0);
+        int C = 0, R = 0;
+        for(int i = 1; i < n-1; i++) {
+            int mirror = 2*C - i;
+            if(i < R)
+                P[i] = min(R - i, P[mirror]);
+            while(t[i + 1 + P[i]] == t[i - 1 - P[i]])
+                P[i]++;
+            if(i + P[i] > R) {
+                C = i;
+                R = i + P[i];
             }
         }
-        return s.substr(a,b-a+1);
-    }
-    void expandFromCenter(int left, int right, const string& s, vector<pair<int,int>>& dp) {
-        while(left>=0 && right<s.size() && s[left]==s[right]) {
-            left--;
-            right++;
+        int maxLen = 0, center = 0;
+        for(int i = 1; i < n-1; i++) {
+            if(P[i] > maxLen) {
+                maxLen = P[i];
+                center = i;
+            }
         }
-        dp.push_back({left+1,right-1});
+        int start = (center - maxLen) / 2;
+        return s.substr(start, maxLen);
     }
 };
