fffce5fef75891f881e1443d8147e2d1ee46f238
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-04-03 00:07:41 +0530

    Time: 0 ms (100%), Space: 31.3 MB (65.01%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
-    void leftShift(vector<int>& arr, int m, int k) {
-        while (k > 0) {
-            int temp = arr[0];
-            for(int i = 0; i<m-1; ++i) {
-                arr[i] = arr[i+1];
-            }
-            arr[m-1] = temp;
-            k--;
-        }
-    }
-    void rightShift(vector<int>& arr, int m, int k) {
-        while(k > 0) {
-            int temp = arr[m-1];
-            for(int i = m-1; i>0; --i) {
-                arr[i] = arr[i - 1];
-            }
-            arr[0] = temp;
-            k--;
-        }
-    }
     bool areSimilar(vector<vector<int>>& mat, int k) {
         int m = mat.size(), n = mat[0].size();
         k = k % n;
-        vector<vector<int>> matCopy = mat;
-        for(int i = 0; i<m; ++i) {
-            if(i%2 == 0) leftShift(matCopy[i], n, k);
-            else rightShift(matCopy[i], n, k);
-        }
-        for(int i = 0; i<m; ++i) {
-            for(int j = 0; j<n; ++j) {
-                if(matCopy[i][j] != mat[i][j]) return false;
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                int newCol;
+                if(i % 2 == 0) newCol = (j + k) % n;
+                else newCol = (j - k + n) % n;
+                if(mat[i][j] != mat[i][newCol]) return false;
             }
         }
         return true;
