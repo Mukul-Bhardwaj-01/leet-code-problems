a4609ff667d328ab47edb8edae5fae92e5803875
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-04-09 00:51:22 +0530

    Time: 84 ms (33.01%), Space: 166.4 MB (25.36%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int B = sqrt(n) + 1;
+        const long long mod = 1e9 + 7;
+
+        vector<long long> mul(n, 1);
+
+        // Large k → direct
+        for (auto &q : queries) {
+            int l = q[0], r = q[1], k = q[2], val = q[3];
+            if (k >= B) {
+                for (int i = l; i <= r; i += k) {
+                    mul[i] = (mul[i] * val) % mod;
+                }
+            }
+        }
+
+        // Small k → process per k
+        for (int k = 1; k < B; k++) {
+            vector<vector<long long>> temp(k);
+
+            for (auto &q : queries) {
+                if (q[2] == k) {
+                    int l = q[0], r = q[1], val = q[3];
+                    for (int i = l; i <= r; i += k) {
+                        mul[i] = (mul[i] * val) % mod;
+                    }
+                }
+            }
+        }
+
+        int xorVal = 0;
+        for (int i = 0; i < n; i++) {
+            long long val = (nums[i] * mul[i]) % mod;
+            xorVal ^= val;
+        }
+
+        return xorVal;
+    }
+};
