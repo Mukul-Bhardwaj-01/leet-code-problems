d71342a21ca65ed798cdf883ed9675b355a10e09
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-04-16 01:53:39 +0530

    Time: 0 ms (100%), Space: 14.7 MB (95.75%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,21 +11,15 @@
  */
 class Solution {
 public:
+    void revPreorder(TreeNode* node, int level, vector<int> &ans) {
+        if(!node) return;
+        if(level == ans.size()) ans.push_back(node -> val);
+        revPreorder(node -> right, level + 1, ans);
+        revPreorder(node -> left, level + 1, ans);
+    }
     vector<int> rightSideView(TreeNode* root) {
-        if(!root) return {};
-        queue<TreeNode*> q;
         vector<int> ans;
-        q.push(root);
-        while(!q.empty()) {
-            int size = q.size();
-            for(int i = 0; i<size; ++i) {
-                TreeNode* node = q.front();
-                q.pop();
-                if(i == size - 1) ans.push_back(node -> val);
-                if(node -> left) q.push(node -> left);
-                if(node -> right) q.push(node -> right);
-            }
-        }
+        revPreorder(root, 0, ans);
         return ans;
     }
 };
