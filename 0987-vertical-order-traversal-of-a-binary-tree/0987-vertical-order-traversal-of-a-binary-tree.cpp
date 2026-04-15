5d9703cd6d284ef68269aff79c374e31c734a5aa
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-04-15 13:15:59 +0530

    Time: 3 ms (40.03%), Space: 16.5 MB (31.21%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,20 +13,20 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         if(!root) return {};
-        queue<pair<TreeNode*,pair<int,int>>> q;
+        stack<pair<TreeNode*,pair<int,int>>> st;
         map<int, map<int, multiset<int>>> helperMap;
-        q.push({root, {0,0}});
-        while(!q.empty()) {
-            auto temp = q.front();
-            q.pop();
+        st.push({root, {0,0}});
+        while(!st.empty()) {
+            auto temp = st.top();
+            st.pop();
             TreeNode* node = temp.first;
             int x = temp.second.first, y = temp.second.second;
             helperMap[x][y].insert(node -> val);
-            if(node -> left != nullptr) {
-                q.push({node -> left, {x - 1, y + 1}});
-            }
             if(node -> right != nullptr) {
-                q.push({node -> right, {x + 1, y + 1}});
+                st.push({node -> right, {x + 1, y + 1}});
+            }
+            if(node -> left != nullptr) {
+                st.push({node -> left, {x - 1, y + 1}});
             }
         }
         vector<vector<int>> ans;
