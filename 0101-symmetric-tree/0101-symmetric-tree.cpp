049cf21a8026d6f4792e603a70e3f62b1bb038db
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-04-16 02:49:02 +0530

    Time: 0 ms (100%), Space: 18.6 MB (26%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -13,23 +13,18 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         if(!root) return true;
-
-        queue<TreeNode*> q1, q2;
-        q1.push(root->left);
-        q2.push(root->right);
-
-        while(!q1.empty() && !q2.empty()) {
-            TreeNode* n1 = q1.front(); q1.pop();
-            TreeNode* n2 = q2.front(); q2.pop();
-
+        queue<TreeNode*> q;
+        q.push(root->left);
+        q.push(root->right);
+        while(!q.empty()) {
+            TreeNode* n1 = q.front(); q.pop();
+            TreeNode* n2 = q.front(); q.pop();
             if(!n1 && !n2) continue;
-            if(!n1 || !n2) return false;
-            if(n1->val != n2->val) return false;
-
-            q1.push(n1->left);
-            q2.push(n2->right);
-            q1.push(n1->right);
-            q2.push(n2->left);
+            if(!n1 || !n2 || n1->val != n2->val) return false;
+            q.push(n1->left);
+            q.push(n2->right);
+            q.push(n1->right);
+            q.push(n2->left);
         }
         return true;
     }
