6adfef76bb8b1609cd6dd2e86ec7ff8b13442683
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-04-22 22:31:10 +0530

    Time: 53 ms (70%), Space: 19.8 MB (84.45%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> seen;
-        if(find(wordList.begin(), wordList.end(), endWord) == wordList.end()) return 0;
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        // If endWord not present, no possible transformation
+        if(st.find(endWord) == st.end()) return 0;
         queue<string> q;
-        int ans = 0;
         q.push(beginWord);
-        seen.insert(beginWord);
-        int n = wordList.size();
+        int steps = 1;
         while(!q.empty()) {
             int size = q.size();
-            ans++;
-            for(int i = 0; i<size; ++i) {
+            while(size--) {
                 string word = q.front();
-                if(word == endWord) return ans;
                 q.pop();
-                int wordLen = word.length();
-                for(int j = 0; j < n; ++j) {
-                    int cnt = 0;
-                    for(int k = 0; k < wordLen; ++k) {
-                        if(wordList[j][k] != word[k]) cnt++;
-                        if(cnt > 1) break;
+                if(word == endWord) return steps;
+                // Try all possible one-letter transformations
+                for(int i = 0; i < word.length(); i++) {
+                    char original = word[i];
+                    for(char ch = 'a'; ch <= 'z'; ch++) {
+                        word[i] = ch;
+                        // If valid word found in dictionary
+                        if(st.find(word) != st.end()) {
+                            q.push(word);
+                            st.erase(word); // mark visited
+                        }
                     }
-                    if(cnt == 1 && seen.find(wordList[j]) == seen.end()) {
-                        q.push(wordList[j]);
-                        seen.insert(wordList[j]);
-                    }
+                    word[i] = original; // restore
                 }
             }
+            steps++;
         }
         return 0;
     }
