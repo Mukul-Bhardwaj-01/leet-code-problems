223c7b5eb64f9d47491da6e2d0160b5341985777
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-04-29 01:47:40 +0530

    Time: 1051 ms (6.17%), Space: 83 MB (81.89%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    int partition(vector<int>& arr, int left, int right) {
+        int pivot = arr[left];
+        int i = left+1, j = right;
+        while(i <= j) {
+            while(i <= right && arr[i] < pivot) i++;
+            while(j > left && arr[j] >= pivot) j--;
+            if(i<j) swap(arr[i],arr[j]);
+        }
+        swap(arr[left],arr[j]);
+        return j;
+    }
+
+    int quickSelect(vector<int>& arr, int left, int right, int k) {
+        if (left == right) return arr[left];
+        int pivotIndex = left + rand() % (right - left + 1);
+        swap(arr[left],arr[pivotIndex]);
+        int pos = partition(arr, left, right);
+
+        if (pos == k) return arr[pos];
+        if (pos > k) return quickSelect(arr, left, pos - 1, k);
+        return quickSelect(arr, pos + 1, right, k);
+    }
+
     int minOperations(vector<vector<int>>& grid, int x) {
         int m = grid.size();
         int n = grid[0].size();
@@ -9,8 +32,7 @@ public:
             for(int j = 0; j<n; ++j)
                 grid1D[i*n + j] = grid[i][j];
         }
-        sort(grid1D.begin(),grid1D.end());
-        int median = grid1D[size/2];
+        int median = quickSelect(grid1D, 0, size - 1, size / 2);
         int res = 0;
         for(int &i : grid1D) {
             if(abs(i - median) % x != 0) return -1;
