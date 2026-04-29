e0f5feb2885a9e5424d6306f1850510ee7ff6a63
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-04-29 11:53:03 +0530

    Time: 27 ms (7.72%), Space: 15.8 MB (52.74%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -14,6 +14,8 @@ public:
     }
     int quickSelect(vector<int>& arr, int low, int high, int targetIndex) {
         if(low == high) return arr[low];
+        int pivotIndex = low + rand() % (high - low + 1);
+        swap(arr[low], arr[pivotIndex]);
         int k = partition(arr,low,high);
         if(k == targetIndex) return arr[k];
         else if(k > targetIndex) return quickSelect(arr, low, k - 1, targetIndex);
