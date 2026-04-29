8d38051eb74e8d0097dbd1e27276044c2c71bf63
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-04-29 11:51:24 +0530

    Time: 78 ms (7.72%), Space: 15.6 MB (98.22%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
+    int partition(vector<int>&arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low + 1, j = high;
+        while(i<=j) {
+            while(i<=high && arr[i] <= pivot) i++;
+            while(j>low && arr[j] > pivot) j--;
+            if(i < j)
+                swap(arr[i],arr[j]);
+        }
+        swap(arr[low],arr[j]);
+        return j;
+    }
+    int quickSelect(vector<int>& arr, int low, int high, int targetIndex) {
+        if(low == high) return arr[low];
+        int k = partition(arr,low,high);
+        if(k == targetIndex) return arr[k];
+        else if(k > targetIndex) return quickSelect(arr, low, k - 1, targetIndex);
+        return quickSelect(arr, k + 1, high, targetIndex);
+    }
     int minMoves2(vector<int>& nums) {
         int n = nums.size(), ans = 0;
-        sort(nums.begin(),nums.end());
-        int median = nums[n/2];
+        int median = quickSelect(nums,0,n-1,n/2);
         for(int &i : nums) {
             ans += abs(i - median);
         }
