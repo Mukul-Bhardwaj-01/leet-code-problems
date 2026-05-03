7e044176e67e8d6651d0ea89c5588312a27a08f0
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-05-04 00:10:04 +0530

    Time: 3 ms (53.18%), Space: 25.6 MB (18.65%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        //row-wise check O(n X n)
-        for(auto &vec : board) {
-            unordered_set<char> s1;
-            for(auto &c : vec) {
+        vector<unordered_set<char>> rows(9);
+        vector<unordered_set<char>> cols(9);
+        vector<unordered_set<char>> boxes(9);
+        for(int i = 0; i < 9; ++i) {
+            for(int j = 0; j < 9; ++j) {
+                char c = board[i][j];
                 if(c == '.') continue;
-                if(s1.count(c)) return false;
-                s1.insert(c);
-            }
-        }
-        //column-wise check O(n X n)
-        for(int i = 0; i<9; ++i) {
-            unordered_set<char> s2;
-            for(int j = 0; j<9; ++j) {
-                if(board[j][i] == '.') continue;
-                if(s2.count(board[j][i])) return false;
-                s2.insert(board[j][i]);
-            }
-        }
-        //3 X 3 box wise checks O(n X n)
-        for(int k = 0; k<3; ++k) {
-            unordered_set<char> seen1, seen2, seen3;
-            for(int i = k*3; i< k*3 + 3; ++i) {
-                for(int j = 0; j<3; ++j) {
-                    if(board[i][j] == '.') continue;
-                    if(seen1.count(board[i][j])) return false;
-                    seen1.insert(board[i][j]);
-                }
-                for(int j = 3; j<6; ++j) {
-                    if(board[i][j] == '.') continue;
-                    if(seen2.count(board[i][j])) return false;
-                    seen2.insert(board[i][j]);
-                }
-                for(int j = 6; j<9; ++j) {
-                    if(board[i][j] == '.') continue;
-                    if(seen3.count(board[i][j])) return false;
-                    seen3.insert(board[i][j]);
-                }
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                if(rows[i].count(c) || cols[j].count(c) || boxes[boxIndex].count(c)) return false;
+                rows[i].insert(c);
+                cols[j].insert(c);
+                boxes[boxIndex].insert(c);
             }
         }
         return true;
