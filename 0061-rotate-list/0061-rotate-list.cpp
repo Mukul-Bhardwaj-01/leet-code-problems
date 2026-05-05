acd3ce90cb908d7595a10d81eeb0d1c262199ee7
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-05-05 12:04:02 +0530

    Time: 0 ms (100%), Space: 16.6 MB (31.2%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -24,35 +24,18 @@ public:
         k = k%n;
         //check for no rotations
         if(k == 0) return head;
-        //stack to store nodes which are to be moved to the front
-        stack<ListNode*> s;
-
-        ListNode* temp2 = head;
-        //number of nodes that stay as is and not needed to be pushed into the stack
-        int asIs = n - k;
+        //make the LL circular
+        temp -> next = head;
+        //connect the current tail to the head and maintain a pointer to the original head and then just go to n - k - 1 th node and call it the newTail, its next will be the new head and then snap the newTail with the newHead and return newHead
+        int newtailPos = n - k - 1;
+        ListNode* newTail = head;
         int cnt = 0;
-        while(cnt < asIs - 1) {
+        while(cnt < newtailPos) {
             cnt++;
-            temp2 = temp2 -> next;
+            newTail = newTail -> next;
         }
-        ListNode* newHead = temp2 -> next;
-        temp2 -> next = nullptr;
-        //temp2 now points to the last node of the final linked list
-        //newHead stores the elements which need to be moved to the front and thus before that to be pushed into the stack
-        while(newHead) {
-            s.push(newHead);
-            newHead = newHead -> next;
-        }
-        //finally newHead is at NULL and now we have a linked list with last k elements missing and those are stored in the stack.
-        //now we will do the operation of pop from stack and make it the new head and connect with the current head of the list until the stack is empty
-        ListNode* tempHead = head;
-        while(!s.empty()) {
-            ListNode* LN1 = s.top();
-            s.pop();
-            ListNode* currentHead = tempHead;
-            tempHead = LN1;
-            tempHead -> next = currentHead;
-        }
-        return tempHead;
+        ListNode* newHead = newTail -> next;
+        newTail -> next = nullptr;
+        return newHead;
     }
 };
