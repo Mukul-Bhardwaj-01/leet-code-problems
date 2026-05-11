cb5e073219571ef434038779b77f185d4b2f728d
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-05-11 11:44:30 +0530

    Time: 0 ms (100%), Space: 15.3 MB (54.47%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    vector<int> separate(int i) {
-        vector<int> sep;
-        while(i > 0) {
-            sep.push_back(i%10);
-            i = i/10;
-        }
-        return sep;
-    }
     vector<int> separateDigits(vector<int>& nums) {
         vector<int> ans;
-        for(int i : nums) {
-            vector<int> temp = separate(i);
-            int tempsize = temp.size();
-            for(int t = tempsize - 1; t>=0; --t) {
-                ans.push_back(temp[t]);
+        for(int &i : nums) {
+            if(i < 10) {
+                ans.push_back(i);
+                continue;
             }
+            string s = to_string(i);
+            for(char &c : s) ans.push_back(c - '0');
         }
         return ans;
     }
