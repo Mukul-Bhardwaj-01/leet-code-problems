56f128409cc14c1651013e55fdfb561285db0177
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-05-13 00:50:37 +0530

    Time: 0 ms (100%), Space: 8.5 MB (5.72%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    unordered_set<int> seen;
-    bool check(int num) {
-        int processed = 0;
-        while(num > 0) {
-            int a = num%10;
-            processed += (a*a);
-            num /= 10;
+    int nextNum(int n) {
+        int sum = 0;
+        while(n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
         }
-        if(!seen.count(processed)) seen.insert(processed);
-        else if(seen.count(processed) && processed == 1) return true;
-        else if(seen.count(processed) && processed != 1) return false;
-        return check(processed);
+        return sum;
     }
     bool isHappy(int n) {
-        return check(n);
+        unordered_set<int> seen;
+        while(n != 1 && !seen.count(n)) {
+            seen.insert(n);
+            n = nextNum(n);
+        }
+        return n == 1;
     }
 };
