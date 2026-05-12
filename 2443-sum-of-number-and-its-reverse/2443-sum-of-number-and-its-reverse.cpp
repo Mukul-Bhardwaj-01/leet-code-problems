f0a78c7653beb32c8dcc9af401bd2523cc123457
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-05-13 01:06:45 +0530

    Time: 65 ms (29.1%), Space: 7.7 MB (68.82%) - LeetHub

--- 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    int rev(int n) {
-        int ans = 0;
-        while(n) {
-            ans = ans*10 + n%10;
-            n /= 10;
-        }
-        return ans;
-    }
     bool sumOfNumberAndReverse(int num) {
         for(int i = 0; i <= num; ++i) {
-            if(i + rev(i) == num) return true;
+            int j = i, rev = 0;
+            while(j) {
+                rev = rev*10 + j%10;
+                j /= 10;
+            }
+            if(i + rev == num) return true;
         }
         return false;
     }
