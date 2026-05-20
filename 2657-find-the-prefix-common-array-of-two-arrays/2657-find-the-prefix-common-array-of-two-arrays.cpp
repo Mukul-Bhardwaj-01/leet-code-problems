210b89448660d939c438f9c2a73333bd7acfcb44
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-05-20 12:37:19 +0530

    Time: 4 ms (64.74%), Space: 85.9 MB (76.5%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    int countCommons(vector<int>& A, vector<int>& B, int i) {
-        unordered_set<int> setOfA;
-        for(int k = 0; k<=i; ++k) {
-            setOfA.insert(A[k]);
-        }
-        int cnt = 0;
-        for(int k = 0; k<=i; ++k) {
-            if(setOfA.count(B[k])) cnt++;
-        }
-        return cnt;
-    }
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int n = A.size();
-        vector<int> C(n,0);
+        int n = A.size(), cnt = 0;
+        vector<int> C(n,0), freq(n+1,0);
         for(int i = 0; i<n; ++i) {
-            C[i] = countCommons(A,B,i);
+            freq[A[i]]++;
+            if(freq[A[i]] == 2) cnt++;
+            freq[B[i]]++;
+            if(freq[B[i]] == 2) cnt++;
+            C[i] = cnt;
         }
         return C;
     }
