b6196e44909a62d057a76dda045ca2d11f17ea9c
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-05-20 12:40:32 +0530

    Time: 10 ms (43.06%), Space: 85.8 MB (85.47%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -4,10 +4,8 @@ public:
         int n = A.size(), cnt = 0;
         vector<int> C(n,0), freq(n+1,0);
         for(int i = 0; i<n; ++i) {
-            freq[A[i]]++;
-            if(freq[A[i]] == 2) cnt++;
-            freq[B[i]]++;
-            if(freq[B[i]] == 2) cnt++;
+            if(++freq[A[i]] == 2) cnt++;
+            if(++freq[B[i]] == 2) cnt++;
             C[i] = cnt;
         }
         return C;
