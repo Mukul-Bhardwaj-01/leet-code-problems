90bb705b8e974ce52bca238fa25051a973fbd6dc
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-05-24 00:57:48 +0530

    Time: 0 ms (100%), Space: 8.2 MB (57.86%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        int i = 0, n = s.length();
+        for(i; i<n; ++i) {
+            if(s[i] == 'a') continue;
+            break;
+        }
+        while(i<n) {
+            if(s[i++] == 'b') continue;
+            return false;
+        }
+        return true;
+    }
+};
