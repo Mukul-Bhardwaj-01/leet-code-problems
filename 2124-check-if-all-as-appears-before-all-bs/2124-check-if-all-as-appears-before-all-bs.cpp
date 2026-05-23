f7287af9de404206e1c1cf403fae3ffa15c12450
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-05-24 01:00:19 +0530

    Time: 0 ms (100%), Space: 8.3 MB (57.86%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     bool checkString(string s) {
-        /*
-        int i = 0, n = s.length();
-        for(i; i<n; ++i) {
-            if(s[i] == 'a') continue;
-            break;
-        }
-        while(i<n) {
-            if(s[i++] == 'b') continue;
-            return false;
-        }
-        return true;
-        */
         return s.find("ba") == string::npos;
     }
 };
