ee08b2ea44c09ace93e80f55e8cc1dfb2320b6b1
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-06-01 19:26:25 +0530

    Time: 15 ms (76.89%), Space: 36.8 MB (46.84%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -5,18 +5,17 @@ public:
         sort(arr.begin(),arr.end());
         int d = arr[1] - arr[0];
         int n = arr.size();
+        
+        //finding the correct value of d
         for(int i = 0; i<n-1; ++i) {
-            if(arr[i+1] - arr[i] < d) {
-                while(ans.size()) {
-                    ans.pop_back();
-                }
-                d = arr[i+1] - arr[i];
+            d = min(d,arr[i+1] - arr[i]);
+        }
+
+        //storing all pairs having difference d
+        for(int i = 0; i<n-1; ++i) {
+            if(arr[i+1] - arr[i] == d) {
                 ans.push_back({arr[i],arr[i+1]});
             }
-            else if(arr[i+1] - arr[i] == d) {
-                ans.push_back({arr[i],arr[i+1]});
-            }
-            else continue;
         }
         return ans;
     }
