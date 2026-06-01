881a29c3da52135d4d8489583c132878d73a28b9
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-06-01 14:39:07 +0530

    Time: 13 ms (39.31%), Space: 32 MB (85.23%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(),nums.end());
         int t = nums.size();
         for(int i = 0; i<t; i+=2) {
-            ans += min(nums[i],nums[i+1]);
+            ans += nums[i];
         }
         return ans;
     }
