dce61fb11d5d25a0752b2189592f3a68fe5570d4
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-06-13 13:44:03 +0530

    Time: 0 ms (100%), Space: 43.7 MB (51.01%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        stringstream s;
+        string s = "";
         for(string &word : words) {
             int weight = 0;
             for(char &c : word) {
                 weight += weights[c - 97];
                 weight %= 26;
             }
-            s << (char)(122 - weight);
+            s += (122 - weight);
         }
-        return s.str();
+        return s;
     }
 };
