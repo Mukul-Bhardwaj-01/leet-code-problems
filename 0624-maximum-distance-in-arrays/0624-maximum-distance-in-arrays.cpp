ec14c0edaa2b85dcae0877ff41857b2d7b6c9e16
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-06-24 13:14:21 +0530

    Time: 4 ms (27.77%), Space: 108.1 MB (41.08%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -3,28 +3,28 @@ public:
     int maxDistance(vector<vector<int>>& arrays) {
         int minEle = INT_MAX, maxEle = INT_MIN;
         int minRow, maxRow;
-        for(int i = 0; i < arrays.size(); ++i) {
+        int n = arrays.size();
+        for(int i = 0; i < n; ++i) {
             if(minEle > arrays[i][0]) {
                 minEle = arrays[i][0];
                 minRow = i;
             }
-            if(maxEle < arrays[i][arrays[i].size() - 1]) {
-                maxEle = arrays[i][arrays[i].size() - 1];
+            if(maxEle < arrays[i].back()) {
+                maxEle = arrays[i].back();
                 maxRow = i;
             }
         }
         if(minRow != maxRow) return maxEle - minEle;
 
         int secondMin = INT_MAX, secondMax = INT_MIN;
-        for(int i = 0; i < arrays.size(); ++i) {
+        for(int i = 0; i < n; ++i) {
             if(i != minRow) {
                 if(secondMin > arrays[i][0]) 
                     secondMin = arrays[i][0];
-                if(secondMax < arrays[i][arrays[i].size()-1]) 
-                    secondMax = arrays[i][arrays[i].size() - 1];
+                if(secondMax < arrays[i].back()) 
+                    secondMax = arrays[i].back();
             }
         }
-
-        return max(abs(secondMin - maxEle), abs(secondMax - minEle));
+        return max(maxEle - secondMin, secondMax - minEle);
     }
 };
