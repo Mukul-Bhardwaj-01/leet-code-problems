c2f62cd4e1bdcf38026065709da130976ae0847c
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-07-03 01:25:00 +0530

    Time: 282 ms (87.49%), Space: 66.9 MB (11.67%) - LeetHub

--- 2888-reshape-data-concatenate/2888-reshape-data-concatenate.py
+++ 2888-reshape-data-concatenate/2888-reshape-data-concatenate.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
-    for i in range(0,len(df2)):
-        df1.loc[len(df1)] = df2.iloc[i]
+    df1 = pd.concat([df1,df2], ignore_index = True)
     return df1
