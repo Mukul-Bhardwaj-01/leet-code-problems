cff73288c1d1e86448b121449c0935e78e627a6a
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-07-02 15:44:32 +0530

    Time: 275 ms (80.4%), Space: 67.4 MB (15.33%) - LeetHub

--- 2883-drop-missing-data/2883-drop-missing-data.py
+++ 2883-drop-missing-data/2883-drop-missing-data.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
-    '''
     students.dropna(axis = 'index', how = 'any' , subset = "name", inplace = True)
     return students
+    
     '''
     students = students.loc[students['name'].notnull()]
     return students
+    '''
