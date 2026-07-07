1dd6fd338b0881f2d5be270a5822ce841c050827
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-07-07 13:53:56 +0530

    Time: 0 ms (100%), Space: 31.2 MB (22.76%) - LeetHub

--- 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.cpp
+++ 3745-maximize-expression-of-three-elements/3745-maximize-expression-of-three-elements.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int maximizeExpressionOfThree(vector<int>& nums) {
-        int firstMaxInd = max_element(nums.begin(),nums.end()) - nums.begin();
-        int secondMaxInd = -1;
-        if(firstMaxInd == 0) {
-            secondMaxInd = max_element(nums.begin() + 1, nums.end()) - nums.begin();
+        int a = INT_MIN, b = INT_MIN;
+        int c = INT_MAX;
+        for (int x : nums) {
+            c = min(c, x);
+            if (x >= a) {
+                b = a;
+                a = x;
+            }
+            else if (x > b) b = x;
         }
-        else if(firstMaxInd == nums.size() - 1) {
-            secondMaxInd = max_element(nums.begin(), nums.end() - 1) - nums.begin();
-        }
-        else {
-            int t1 = max_element(nums.begin(), nums.begin() + firstMaxInd) - nums.begin();
-            int t2 = max_element(nums.begin() + firstMaxInd + 1, nums.end()) - nums.begin();
-            if(nums[t1] > nums[t2]) secondMaxInd = t1;
-            else secondMaxInd = t2;
-        }
-        return nums[firstMaxInd] + nums[secondMaxInd] - *min_element(nums.begin(),nums.end());
+        return a + b - c;
     }
 };
