767b4924ca5bb5f1b63ba56502ce840e084c6e99
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-07-12 13:46:03 +0530

    Time: 0 ms (100%), Space: 15.3 MB (15.01%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        int maxi = *max_element(nums.begin(),nums.end());
-        vector<int> count(maxi + 1,0);
-        for(auto &i : nums) {
-            count[i]++;
+        int lesser = 0, equal = 0;
+        for(int & i : nums) {
+            if(i < target) lesser++;
+            else if(i == target) equal++;
         }
-        vector<int> ans;
-        int ind = 0;
-        for(int i = 0; i <= maxi; ++i) {
-            if(i < target && count[i] > 0) {
-                ind += count[i];
-            }
-            else if(i == target) {
-                for(int j = ind; j < ind + count[i]; ++j) {
-                    ans.push_back(j);
-                }
-                break;
-            }
+
+        vector<int> ans(equal,0);
+        for(int i = lesser, ind = 0; i < lesser + equal; ++i, ++ind) {
+            ans[ind] = i;
         }
         return ans;
     }
