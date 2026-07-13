d67cf0912e583c7d89ce0789aa0d531b3a5c3e80
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-07-13 23:29:23 +0530

    Time: 0 ms (100%), Space: 10.1 MB (79.42%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,40 +1,27 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int i = 0;
-        while(!s.empty() && s.back() == ' ') s.pop_back();
-        while(i<(int)s.size() && s[i] == ' ') i++;
-        s.erase(s.begin(),s.begin()+i);
-        if(s.empty()) return "";
-        int n = (int)s.size();
-        int read = 0, write = 0;
-        bool isPrevSpace = false;
-        while(read<n) {
-            if(s[read]!=' ') {
-                s[write++] = s[read++];
-                isPrevSpace = false;
-            }
-            else if(s[read] == ' ' && !isPrevSpace) {
-                s[write++] = s[read++];
-                isPrevSpace = true;
-            }
-            else if(s[read] == ' ' && isPrevSpace) {
-                read++;
-            }
-        }
-        s.resize(write);
-        n=write;
+        while(s.length() > 0 && isspace(s[s.length() - 1])) s.pop_back();
         reverse(s.begin(),s.end());
-        int start = 0, space = 0;
-        while(space<n) {
-            if(s[space] != ' ') space++;
-            else if(s[space] == ' ') {
-                reverse(s.begin()+start, s.begin()+space);
-                space++;
-                start=space;
+        while(s.length() > 0 && isspace(s[s.length() - 1])) s.pop_back();
+        string ans = "";
+        int n = s.length();
+        int i = 0, j = 0;
+        while(j < n) {
+            while(j < n && !isspace(s[j])) j++;
+            string temp = s.substr(i,j-i);
+            reverse(temp.begin(),temp.end());
+            ans += temp + " ";
+            i = j;
+            while(i < n) {
+                if(isspace(s[i])) i++;
+                else {
+                    j = i;
+                    break;
+                }
             }
         }
-        reverse(s.begin()+start, s.begin()+space);
-        return s;
+        ans.pop_back();
+        return ans;
     }
 };
