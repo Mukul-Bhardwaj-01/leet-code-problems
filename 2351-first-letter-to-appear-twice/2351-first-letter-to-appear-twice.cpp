48d80c148c4724f2a1836546de534a50fa6e4422
Mukul-Bhardwaj-01 <cadetmike01@gmail.com>
2026-08-15 23:33:08 +0530

    Time: 0 ms (100%), Space: 8.3 MB (42%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     char repeatedCharacter(string s) {
         vector<int> freq(26,0);
+        char ans;
         for(char &ch : s) {
-            if(freq[ch - 97]) return ch;
+            if(freq[ch - 97]) {
+                ans = ch;
+                break;
+            }
             freq[ch - 97]++;
         }
-        return 'a';
+        return ans;
     }
 };
